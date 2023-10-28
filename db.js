951aabd952e4a1a367ffa2bf108f9d66a461c344
TriptpalSingh <triptpalsingh721@gmail.com>
2023-10-28 22:56:46 +0530

    this is final

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 function connectDb(){
-    mongoose.connect('mongodb+srv://tript:tript@optimus.jprmvx3.mongodb.net/test?retryWrites=true&w=majority').then(()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("connected to db");
     }).catch((e)=>{
         console.log("an error occured");
