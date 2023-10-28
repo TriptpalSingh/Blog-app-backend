6e60afdabdcc67bb60994520b46dd2722f28ce1c
TriptpalSingh <triptpalsingh721@gmail.com>
2023-10-28 22:33:16 +0530

    final final commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 function connectDb(){
-    mongoose.connect('mongodb+srv://tript:tript@optimus.jprmvx3.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+    mongoose.connect('mongodb+srv://tript:tript@optimus.jprmvx3.mongodb.net/test?retryWrites=true&w=majority').then(()=>{
         console.log("connected to db");
     }).catch((e)=>{
         console.log("an error occured");
